3a9253b2ec6218d0ef602b3d0e618e95712e1419
hithachoudhary <hithachoudhary5559@gmail.com>
2024-06-10 00:33:05 +0530

    Uploaded solution files for Task-6
    The folder consists of the corresponding python and sql file addressing problem statement-6 and a schema diagram pdf

--- Source Code/Task6/Data Warehousing.sql
+++ Source Code/Task6/Data Warehousing.sql
@@ -1,44 +1,59 @@
--- Sports Data Analysis : Problem Statement 6
+-- Sports Data Analysis: Problem Statement 6
 
--- 1. Creating tables for the database schema
--- Creation of PlayerDetails table which contains the details of the players
-CREATE TABLE PlayerDetails 
+-- Using the database
+use sports_data;
+
+-- Creating tables for the database
+-- Creating table "PlayerInfo" which stores the details about the players
+CREATE TABLE PlayerInfo 
 (ID INT PRIMARY KEY, 
 Player Varchar(255) NOT NULL, 
 Team VARCHAR(255), 
 Age INT, 
-Height DECIMAL(5,2), 
-Weight DECIMAL(5,2), 
-Position VARCHAR(255), 
-Season VARCHAR(255));
+Height DECIMAL(6,3), 
+Weight DECIMAL(6,3), 
+Position VARCHAR(255));
 
--- Creation of PlayerMatchStats table which contains the details about the players match statistics                 
-CREATE TABLE PlayerMatchStats 
+-- Creatng table "PlayerMatchInfo" to store the match details of the players
+CREATE TABLE PlayerMatchInfo 
 (ID INT, 
 Player VARCHAR(255) NOT NULL,
+Season VARCHAR(255),
 Goals INT, 
 Assists INT, 
 YellowCards INT, 
 RedCards INT, 
-PassCompletionRate DECIMAL(6,2), 
-DistanceCovered DECIMAL(5,2), 
+FOREIGN KEY(ID) REFERENCES PlayerInfo(ID));
+
+-- Creating table "PlayerMatchPerformace" to store the performance of each player during a match
+Create table PlayerMatchPerformance
+(ID INT, 
+Player VARCHAR(255) NOT NULL,
+PassCompletionRate DECIMAL(6,3), 
+DistanceCovered DECIMAL(5,3), 
 Sprints INT, 
 ShotsOnTarget INT, 
 TacklesWon INT, 
 CleanSheets INT, 
-FOREIGN KEY(ID) REFERENCES PlayerDetails(ID));
+FOREIGN KEY(ID) REFERENCES PlayerInfo(ID));
 
--- Creation of PlayerPerformanceMetrics table which contains the players performance metrics    
-CREATE TABLE PlayerPerformanceMetrics 
+-- Creating table "PlayerAttributes" that contains extra information about the players 
+CREATE TABLE PlayerAttributes 
 (ID INT, 
-Player VARCHAR(255) NOT NULL,
-GoalsPerMatch DECIMAL(5,4), 
-AssistsPerMatch DECIMAL(6,5), 
-PlayerFatigue DECIMAL(5,4), 
+Player VARCHAR(255) NOT NULL, 
+PlayerFatigue DECIMAL(4,3), 
 MatchPressure INT,
 InjuryHistory INT,
 TrainingHours DECIMAL(5,3),
-FatigueInjuryCorrelation DECIMAL(5,4), 
-PressurePerformanceImpact DECIMAL(5,4), 
+FatigueInjuryCorrelation DECIMAL(4,3), 
+PressurePerformanceImpact DECIMAL(5,3), 
 EffectiveTraining DECIMAL(6,3),
-FOREIGN KEY(ID) REFERENCES PlayerDetails(ID));
+FOREIGN KEY(ID) REFERENCES PlayerInfo(ID));
+
+-- Creating table "OverallPerformance" to store the overall performance of the players 
+CREATE TABLE OverallPerformance 
+(ID INT,
+Player VARCHAR(255) NOT NULL,
+PerformanceIndex INT,
+GoalScoringEfficiency DECIMAL(4,3),
+FOREIGN KEY (ID) REFERENCES PlayerInfo(ID));
